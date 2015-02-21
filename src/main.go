70b0035a2d24783baa892eb74e3d3f8fbebac625
Nong Anuchit Prasertsang <anuchit.prasertsang@gmail.com>
2015-02-21 16:56:50 +0700

    Group router v1

--- src/main.go
+++ src/main.go
@@ -20,56 +20,63 @@ func main() {
 	router := gin.Default()
 	router.GET("/", rootPath)
 
-	router.GET("/api/v1/:key/todoService", func(c *gin.Context) {
-		service.TodoService()
-		c.String(200, "invork todoService by resource")
-	})
+	v1 := router.Group("/api/v1/:key")
+	{
+			v1.GET("/todoService", func(c *gin.Context) {
+					service.TodoService()
+					c.String(200, "invork todoService by resource")
+				})
 
-	router.GET("/api/v1/:key/pathPrarams/:name", func(c *gin.Context) {
-		fmt.Println("GET /:name ")
-		name := c.Params.ByName("name")
+			v1.GET("/pathParams/:name", func(c *gin.Context) {
+						fmt.Println("GET /:name ")
+						name := c.Params.ByName("name")
+						key := c.Params.ByName("key")
 
-		c.String(200, "hello name:"+name)
-	})
+						c.String(200, "hello name:"+name+"  key:"+key)
+					})
 
-	router.GET("/api/v1/:key/queryParams", func(c *gin.Context) {
-		fmt.Println("GET /api/v1/:key/queryParams")
-		token := c.Request.URL.Query().Get("token")
-		c.String(200, "hello "+token)
-	})
+			v1.GET("/queryParams", func(c *gin.Context) {
+						fmt.Println("GET /queryParams")
+						token := c.Request.URL.Query().Get("token")
+						c.String(200, "hello "+token)
+					})
 
-	router.GET("/api/vi/:key/json", func(c *gin.Context) {
-		var msg struct {
-			Name    string `json:"user"`
-			Message string
-			Number  int
-		}
+			v1.GET("/json", func(c *gin.Context) {
+						var msg struct {
+							Name    string `json:"user"`
+							Message string
+							Number  int
+						}
 
-		msg.Name = "Nong"
-		msg.Message = "hello Json"
-		msg.Number = 123456789
+						msg.Name = "Nong"
+						msg.Message = "hello Json"
+						msg.Number = 123456789
 
-		c.JSON(200, msg)
-	})
+						c.JSON(200, msg)
+					})
 
-	type UserJSON struct {
-		UserName string `json:"user" binding:"required"`
-		Phone    string
+					type UserJSON struct {
+						UserName string `json:"user" binding:"required"`
+						Phone    string
+					}
+
+					/*{
+						"user":"Anuchit",
+						"phone":"1234567897"
+
+					}
+					*/
+			v1.POST("/pass/json", func(c *gin.Context) {
+						var json UserJSON
+
+						if c.EnsureBody(&json) {
+							c.JSON(200, json)
+						}
+					})
 	}
 
-	/*{
-	  	"user":"Anuchit",
-	 	"phone":"1234567897"
 
-	}
-	*/
-	router.POST("/api/v1/:key/pass/json", func(c *gin.Context) {
-		var json UserJSON
 
-		if c.EnsureBody(&json) {
-			c.JSON(200, json)
-		}
-	})
 
 	router.Run(":8080")
 
